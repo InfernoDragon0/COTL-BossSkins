7460303855aa4b3b61b9f61665948aa8f399b526
InfernoDragon0 <mushroomapplepiele@gmail.com>
2024-07-25 14:01:13 +0800

    initial commit

---
+++ Patches/SkinSelectorPatch.cs
@@ -0,0 +1,57 @@
+﻿using COTL_API.CustomTarotCard;
+using HarmonyLib;
+using Lamb.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BossSkins.Patches
+{
+    [HarmonyPatch]
+    internal class SkinSelectorPatch
+    {
+        [HarmonyPatch(typeof(UIAppearanceMenuController_Form), nameof(UIAppearanceMenuController_Form.OnShowStarted))]
+        [HarmonyPostfix]
+        public static void UIAppearanceMenuController_Form_OnShowStarted(UIAppearanceMenuController_Form __instance)
+        {
+            //Get all the boss skins
+            //convert the above to a list
+            var skinList = new List<int>()
+            {
+                WorshipperData._Instance.GetSkinIndexFromName("Boss Death Cat"),
+                WorshipperData._Instance.GetSkinIndexFromName("Boss Aym"),
+                WorshipperData._Instance.GetSkinIndexFromName("Boss Baal"),
+                WorshipperData._Instance.GetSkinIndexFromName("CultLeader 1"),
+                WorshipperData._Instance.GetSkinIndexFromName("CultLeader 2"),
+                WorshipperData._Instance.GetSkinIndexFromName("CultLeader 3"),
+                WorshipperData._Instance.GetSkinIndexFromName("CultLeader 4"),
+                WorshipperData._Instance.GetSkinIndexFromName("Sozo"),
+
+            };
+
+            var list = new List<WorshipperData.SkinAndData>()
+            {
+            };
+
+            foreach ( var skinid in skinList )
+            {
+                Plugin.Log.LogInfo("BOSS SKIN Enable ID " + skinid);
+                list.Add(WorshipperData._Instance.Characters[skinid]);
+
+            }
+
+            __instance._specialEventsHeader.SetActive(true);
+            __instance._formItems.AddRange(
+                __instance.Populate(list, __instance._specialEventsContent, null)
+            );
+
+            /*return true;*/
+        }
+
+
+
+        
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,45 @@
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.IO;
+
+namespace BossSkins
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVer)]
+    [BepInDependency("io.github.xhayper.COTL_API")]
+    [HarmonyPatch]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PluginGuid = "InfernoDragon0.cotl.BossSkins";
+        public const string PluginName = "BossSkins";
+        public const string PluginVer = "1.0.0";
+
+        internal static ManualLogSource Log;
+        internal readonly static Harmony Harmony = new(PluginGuid);
+
+        internal static string PluginPath;
+  
+
+        private void Awake()
+        {
+            Plugin.Log = base.Logger;
+
+            PluginPath = Path.GetDirectoryName(Info.Location);
+
+            //SETUP: Config
+
+        }
+
+        private void OnEnable()
+        {
+            Harmony.PatchAll();
+            Logger.LogInfo($"Loaded {PluginName}!");
+        }
+
+        private void OnDisable()
+        {
+            Harmony.UnpatchSelf();
+            Logger.LogInfo($"Unloaded {PluginName}!");
+        }
+    }
+}
